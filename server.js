18de37239569abb382a500337cf483dbb4361a38
Ameen Reda <ameenreda53@gmail.com>
2021-02-08 17:36:29 +0200

    updates in server.js

--- server.js
+++ server.js
@@ -1,39 +1,30 @@
-`use Strict`;
+////////////////////////////////old-commit----////////////////////////////////////
+
 if(process.env.NODE_ENV !='production'){
     require('dotenv').config();
-}
+	}
 
-const express = require('express');
-const { connect } = require('mongoose');
-const expressLayout = ('express-ejs-layouts');
-//IMPORTED ROUTE
-const indexRouter = require('./routes/index')
 
-const app = express();
+const express=require('express');	
+const mongoose=require('mongoose');	
 
-connect(process.env.DATABASE_URL,{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true
-}, (err) => {
-        if (err) console.log("Error connecting to Database")
-        else console.log("Connected to database successfully");
+mongoose.connect(process.env.DATABASE_URL,{	
+    useNewUrlParser:true	//IMPORTED ROUTE
 })
-
-
-app.set('view engine', 'ejs');
-app.set('views', 'views')
-app.set('layout', 'layouts/layout')
-
+const db=mongoose.connection
+db.on('error',error=>{
+    console.log(error);
+})
+db.once('open',()=>{
+    console.log('connected to  mongoose');
+})
+const app=express();
+const expressLayout=require('express-ejs-layouts');
+const indexRouter=require('./routes/index')
+app.set('view engine','ejs');
 app.use(expressLayout);
+app.set('views','views')
 app.use(express.static('public'))
-//ROUTE MIDDLEWARE
-app.use('/',indexRouter);
-
-const PORT = process.env.PORT ||3000
-
-//Server listening  
-app.listen(PORT, (err) => {
-    if (err) console.log(`Error connecting server on port:${PORT}`)
-    console.log(`Server connected on port ${PORT}`);
-});
+app.set('layout','layouts/layout')
+app.use('',indexRouter);
+app.listen(process.env.PORT ||3000);
