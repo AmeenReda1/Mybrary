0b1b0894ad96ce9036132d2c06d370fdffbdbf2d
Ameen Reda <ameenreda53@gmail.com>
2021-02-18 02:37:46 +0200

    initial author routes

---
+++ routes/author.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const auhtor = require('../models/auhtor');
+const router = express.Router();
+const Author=require('../models/auhtor')
+
+//All Author routes
+router.get('/',(req,res,next)=>{
+    let searchOption={}
+    if(req.query.name !=null && req.query.name!==''){
+        searchOption.name=new RegExp(req.query.name,'i')
+    }
+    try{
+        Author.find(searchOption)
+        .exec()
+        .then(authors=>{
+            console.log(authors)
+            res.render('authors/index',{
+                
+                authors:authors,
+                searchOption:req.query
+            }
+            )
+        })
+    }
+    catch{
+        
+    }
+    
+})
+router.get('/new',(req,res)=>{
+    res.render('authors/new',{authors:new Author() })
+  
+})
+//create Author route
+router.post('/',(req,res,next)=>{
+    const author=new Author({
+        name:req.body.name,
+        
+    })
+    author.save()
+    console.log('hi from req.name',req.body.name)
+    
+    
+        res.render('authors/new',{
+            authors:author,
+            errorMessage:'Error create Author'
+            })
+    
+    // author.save()
+    // .then(auhtor=>{
+    //     if(author){
+    //         // res.redirect(`authors/${newAuthor.id}`)
+    //         res.redirect(`authors`)
+    //     }
+    //     else{
+            
+    //         res.render('auhtors/new',{
+    //             auhtor:'',
+    //             errorMessage:'Error create Author'
+    //         })
+    //     }
+    // })
+ 
+})
+module.exports = router;
--- server.js
+++ server.js
@@ -8,6 +8,10 @@ if(process.env.NODE_ENV !='production'){
 const express=require('express');	
 const mongoose=require('mongoose');	
 
+const indexRouter=require('./routes/index')
+const authorRouter=require('./routes/author')
+
+
 mongoose.connect(process.env.DATABASE_URL,{	
     useNewUrlParser:true	//IMPORTED ROUTE
 })
@@ -20,11 +24,22 @@ db.once('open',()=>{
 })
 const app=express();
 const expressLayout=require('express-ejs-layouts');
-const indexRouter=require('./routes/index')
+const bodyParser=require('body-parser')
 app.set('view engine','ejs');
 app.use(expressLayout);
 app.set('views','views')
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({limit:'10mb',extended:false}))
 app.set('layout','layouts/layout')
-app.use('',indexRouter);
+
+
+app.use('',indexRouter); 
+app.use('/authors',authorRouter); 
+
+
 app.listen(process.env.PORT ||3000);
+
+
+
+
+
